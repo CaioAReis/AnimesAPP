63354319e19e9c49a949eaaf2dff13f2442542c2
CaioAReis <caio.almeida16.ca@gmail.com>
2024-01-26 14:58:25 -0300

    Detalhes

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,45 +1,74 @@
-import { AtSignIcon, CalendarDaysIcon, FavouriteIcon, Icon, MessageCircleIcon } from "@gluestack-ui/themed";
 import { Tabs } from "expo-router";
+import { BlurView } from "expo-blur";
+import { Center, Icon } from "@gluestack-ui/themed";
+import { Heart, Home, LucideIcon, Search, UserRound } from "lucide-react-native";
+
+interface TabButtonProps {
+  size?: number,
+  color?: string,
+  icon: LucideIcon,
+  focused?: boolean,
+}
+
+const TabButton = ({ color, icon }: TabButtonProps) => (
+  <BlurView intensity={100} style={{ width: "100%", height: "100%" }}>
+    <Center h="$full" w="$full">
+      <Icon as={icon} color={color} />
+    </Center>
+  </BlurView>
+);
 
 export default function TabsApp() {
 
   return (
-    <Tabs backBehavior="none" initialRouteName="home/index" screenOptions={{ headerShown: false }}>
+    <Tabs
+      backBehavior="none"
+      initialRouteName="home/index"
+      sceneContainerStyle={{ backgroundColor: "white" }}
+      screenOptions={{
+        headerShown: false,
+        freezeOnBlur: true,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          elevation: 0,
+          borderColor: "transparent",
+          backgroundColor: "transparent",
+        },
+      }}
+    >
+
       <Tabs.Screen
-        name="home/index"
+        name="home"
         options={{
           title: "INÍCIO",
-          freezeOnBlur: true,
-          tabBarIcon: ({ color }) => <Icon as={CalendarDaysIcon} color={color} />,
+          tabBarIcon: ({ color }) => <TabButton icon={Home} color={color} />,
         }}
       />
 
       <Tabs.Screen
-        name="search/index"
+        name="search"
         options={{
           title: "PESQUISAR",
-          freezeOnBlur: true,
-          tabBarIcon: ({ color }) => <Icon as={MessageCircleIcon} color={color} />,
+          tabBarIcon: ({ color }) => <TabButton icon={Search} color={color} />,
         }}
       />
 
       <Tabs.Screen
-        name="library/index"
+        name="library"
         options={{
           title: "SALVOS",
-          freezeOnBlur: true,
-          tabBarIcon: ({ color }) => <Icon as={FavouriteIcon} color={color} />,
+          tabBarIcon: ({ color }) => <TabButton icon={Heart} color={color} />,
         }}
       />
 
       <Tabs.Screen
-        name="account/index"
+        name="account"
         options={{
           title: "PERFIL",
-          freezeOnBlur: true,
-          tabBarIcon: ({ color }) => <Icon as={AtSignIcon} color={color} />,
+          tabBarIcon: ({ color }) => <TabButton icon={UserRound} color={color} />,
         }}
       />
+
     </Tabs>
   );
 }
