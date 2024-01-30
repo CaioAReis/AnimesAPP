a8a474ff61321d869862424a92d6e79052102ff3
CaioAReis <caio.almeida16.ca@gmail.com>
2024-01-30 11:55:56 -0300

    Flatlist na lista horizontal

--- src/app/(tabs)/home/components/HorizontalList.tsx
+++ src/app/(tabs)/home/components/HorizontalList.tsx
@@ -1,4 +1,5 @@
-import { Box, HStack, Heading, ScrollView } from "@gluestack-ui/themed";
+import { FlatList } from "react-native";
+import { Box, Heading } from "@gluestack-ui/themed";
 
 import { AnimeCard } from "../../../../components";
 import { AnimeCardProps } from "../../../../components/AnimeCard";
@@ -13,22 +14,24 @@ export default function HorizontalList({ title, list }: HorizontalListProps) {
   return (
     <Box>
       <Heading ml={20} my={15}>{title}</Heading>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <HStack space="md">
-          <Box w={10} />
 
-          {list?.map((item, i) => (
-            <AnimeCard
-              key={i}
-              title={item.title}
-              image={item?.image}
-              description={item?.description}
-            />
-          ))}
+      <FlatList
+        horizontal
+        data={list}
+        maxToRenderPerBatch={3}
+        ListHeaderComponent={<Box w={20} />}
+        ListFooterComponent={<Box w={20} />}
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={() => <Box w={10} />}
+        renderItem={({ item }) => (
+          <AnimeCard
+            title={item.title}
+            image={item?.image}
+            description={item?.description}
+          />
+        )}
+      />
 
-          <Box w={10} />
-        </HStack>
-      </ScrollView>
     </Box>
   );
 }
