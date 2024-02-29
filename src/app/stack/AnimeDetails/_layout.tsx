ccf76efba355fcb36beb42a176cf09b337fc6fe2
CaioAReis <caio.almeida16.ca@gmail.com>
2024-02-29 10:00:13 -0300

    Detalhes

--- src/app/stack/AnimeDetails/_layout.tsx
+++ src/app/stack/AnimeDetails/_layout.tsx
@@ -1,5 +1,6 @@
-import { Box, Text } from "@gluestack-ui/themed";
+import { Box, HStack, Heading, Icon, Text } from "@gluestack-ui/themed";
 import { CoverHeader } from "./components";
+import { Star } from "lucide-react-native";
 
 export default function AnimeDetails() {
 
@@ -10,6 +11,23 @@ export default function AnimeDetails() {
         onPlay={() => alert("Foi")}
         imageCover="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.enjpg.com%2Fimg%2F2020%2Fdemon-slayer-desktop-8.jpg&f=1&nofb=1&ipt=4aca62af7b0a9a9be0549fb295ab0f8150c2a63652208498a3bfb572f97de0c4&ipo=images"
       />
+
+      {/* ANIMES DETAILS */}
+      <Box my={15} mx={20}>
+        <Heading>ATTACK ON TITANS</Heading>
+        <Text fontSize="$sm" lineHeight="$sm">2020 | action, adventure, Titans</Text>
+        <HStack mt={8} space="md" alignItems="center">
+          <Text>4.9</Text>
+          <HStack space="xs">
+            <Icon as={Star} />
+            <Icon as={Star} />
+            <Icon as={Star} />
+            <Icon as={Star} />
+            <Icon as={Star} />
+          </HStack>
+        </HStack>
+
+      </Box>
 
       <Text>TELA DE DETALHES</Text>
     </Box>
--- src/app/stack/AnimeDetails/components/CoverHeader.tsx
+++ src/app/stack/AnimeDetails/components/CoverHeader.tsx
@@ -1,7 +1,8 @@
+import { router } from "expo-router";
 import { useWindowDimensions } from "react-native";
-import { ArrowLeft, Play } from "lucide-react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { Button, ButtonIcon, ImageBackground } from "@gluestack-ui/themed";
+import { ArrowLeft, Heart, Play, Share } from "lucide-react-native";
+import { Button, ButtonIcon, HStack, ImageBackground } from "@gluestack-ui/themed";
 
 interface CoverHeaderProps {
   imageCover: string,
@@ -15,7 +16,7 @@ export function CoverHeader({ imageCover, onPlay }: CoverHeaderProps) {
     <ImageBackground
       alt="Anime cover"
       bgColor="$blueGray300"
-      w={width} h={height / 1.4}
+      w={width} h={height / 1.5}
       source={{ uri: imageCover }}
     >
       <LinearGradient style={{ flex: 1 }} colors={["transparent", "transparent", "white"]}>
@@ -27,21 +28,41 @@ export function CoverHeader({ imageCover, onPlay }: CoverHeaderProps) {
           rounded="$full"
           borderWidth={2}
           borderColor="$red500"
+          onPress={() => router.back()}
         >
           <ButtonIcon as={ArrowLeft} size="xl" color="$red500" />
         </Button>
 
-        <Button
-          right={40}
-          bottom={-20}
-          w={80} h={80}
-          rounded="$full"
-          onPress={onPlay}
-          bgColor="$red500"
-          position="absolute"
-        >
-          <ButtonIcon as={Play} size="xl" color="white" />
-        </Button>
+        <HStack justifyContent="space-between" w="$full" px={20} position="absolute" bottom={-20}>
+          <HStack space="md">
+            <Button
+              w={40} h={40}
+              rounded="$full"
+              onPress={onPlay}
+              bgColor="$red700"
+            >
+              <ButtonIcon as={Share} size="md" color="white" />
+            </Button>
+
+            <Button
+              w={40} h={40}
+              rounded="$full"
+              onPress={onPlay}
+              bgColor="$red700"
+            >
+              <ButtonIcon as={Heart} size="xl" color="white" />
+            </Button>
+          </HStack>
+
+          <Button
+            w={80} h={80}
+            rounded="$full"
+            onPress={onPlay}
+            bgColor="$red500"
+          >
+            <ButtonIcon as={Play} size="xl" color="white" />
+          </Button>
+        </HStack>
 
       </LinearGradient>
     </ImageBackground>
