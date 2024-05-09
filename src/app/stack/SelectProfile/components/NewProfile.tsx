d6c06f177bd3af32fd791a9da09df9b87de18759
CaioAReis <caio.almeida16.ca@gmail.com>
2024-05-09 14:59:09 -0300

    Form na criação do perfil

--- src/app/stack/SelectProfile/components/NewProfile.tsx
+++ src/app/stack/SelectProfile/components/NewProfile.tsx
@@ -1,3 +1,4 @@
+import { Controller, useForm } from "react-hook-form";
 import { AlertCircleIcon, SmilePlus } from "lucide-react-native";
 import {
   Box,
@@ -9,6 +10,7 @@ import {
   HStack,
   Avatar,
   Heading,
+  useToken,
   ButtonText,
   InputField,
   AvatarImage,
@@ -27,18 +29,32 @@ import {
   ActionsheetDragIndicatorWrapper,
 } from "@gluestack-ui/themed";
 
+import { regexs } from "@/config/utils";
+
 interface NewProfileProps {
   isOpen: boolean,
   onClose: () => void,
 }
 
 export function NewProfile({ isOpen, onClose }: NewProfileProps) {
+  const switchSuccess = useToken("colors", "primary500");
+  const switchBg = useToken("colors", "bg200" as "primary500");
+  const switchThumb = useToken("colors", "primary50" as "primary500");
 
-  const onSubmit = () => {
+  interface formType {
+    name: string,
+    isChild: boolean,
+  }
+
+  const { control, handleSubmit, watch, formState: { errors }, } = useForm({
+    defaultValues: { name: "", isChild: false },
+  });
+
+  const onSubmit = (data: formType) => {
+    console.warn(data);
 
     alert("CREATE A NEW PROFILE");
     onClose();
-
   };
 
   return (
@@ -56,55 +72,77 @@ export function NewProfile({ isOpen, onClose }: NewProfileProps) {
             <Avatar my={20} alignSelf="center" bgColor="transparent" size="xl" borderRadius="$full">
               <AvatarFallbackText>{"CAIO"}</AvatarFallbackText>
               <AvatarImage
-                alt={`${"CAIO"}'s profile`}
-                source={{ uri: "https://api.dicebear.com/8.x/bottts/png?seed=C" }}
+                alt={`${watch("name")}'s profile`}
+                source={{ uri: `https://api.dicebear.com/8.x/bottts/png?seed=${watch("name")}` }}
               />
             </Avatar>
 
-            <FormControl
-              w="$full"
-              size="md"
-              alignSelf="center"
-              isDisabled={false}
-              isInvalid={false}
-              isReadOnly={false}
-              isRequired={false}
-            >
-              <FormControlLabel mb="$1">
-                <FormControlLabelText>Name</FormControlLabelText>
-              </FormControlLabel>
-              <Input size="lg">
-                <InputField placeholder="Full name" />
-              </Input>
+            <Controller
+              name="name"
+              control={control}
+              rules={{
+                required: "The name is required",
+                pattern: { value: regexs.NAME, message: "Name invalid." }
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <FormControl
+                  w="$full"
+                  size="lg"
+                  isRequired={true}
+                  alignSelf="center"
+                  isInvalid={Boolean(errors?.name)}
+                >
+                  <FormControlLabel mb="$1">
+                    <FormControlLabelText color={errors?.name ? "$error700" : "$bg700"}>
+                      Name
+                    </FormControlLabelText>
+                  </FormControlLabel>
 
-              <FormControlError>
-                <FormControlErrorIcon as={AlertCircleIcon} />
-                <FormControlErrorText>
-                  At least 6 characters are required.
-                </FormControlErrorText>
-              </FormControlError>
-            </FormControl>
+                  <Input size="lg" variant="underlined">
+                    <InputField
+                      value={value}
+                      onBlur={onBlur}
+                      placeholder="Name"
+                      onChangeText={onChange}
+                    />
+                  </Input>
 
-            <HStack justifyContent="flex-end" space="md" my={10} alignItems="center">
-              <Text>Is a child?</Text>
-              <Switch
-                size="lg"
-                defaultValue={true}
-                thumbColor="$success400"
-                trackColor={{
-                  true: "$success200",
-                  false: "$bg100"
-                }}
-              />
-            </HStack>
+                  {errors?.name && (
+                    <FormControlError>
+                      <FormControlErrorIcon as={AlertCircleIcon} />
+                      <FormControlErrorText>
+                        {errors?.name?.message}
+                      </FormControlErrorText>
+                    </FormControlError>
+                  )}
+                </FormControl>
+              )}
+            />
 
-            <Button rounded="$full" mt={20} bgColor="$primary400" onPress={onSubmit}>
+            <Controller
+              name="isChild"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <HStack justifyContent="flex-end" space="md" my={10} alignItems="center">
+                  <Text>Is a child?</Text>
+                  <Switch
+                    size="lg"
+                    onChange={onChange}
+                    defaultValue={value}
+                    thumbColor={switchThumb}
+                    trackColor={{ true: switchSuccess, false: switchBg }}
+                  />
+                </HStack>
+              )}
+            />
+
+            <Button rounded="$full" mt={20} bgColor="$primary400" onPress={handleSubmit(onSubmit)}>
               <ButtonText mx={10} color="$bg0">Create new profile</ButtonText>
               <Icon as={SmilePlus} size="lg" color="$bg0" />
             </Button>
 
             <Button variant="link" mt={20} onPress={onClose}>
-              <ButtonText color="$secondary300" mx={20}>Cancel</ButtonText>
+              <ButtonText color="$error400" mx={20}>Cancel</ButtonText>
             </Button>
           </Box>
         </KeyboardAvoidingView>
